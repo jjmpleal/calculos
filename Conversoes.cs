7b06125d3e26d591c166e6f690d6cecb595697a7
João Leal <134593307+jjmpleal@user.noreply.github.com>
2023-10-10 22:38:06 +0100

    Adicionar o método ConverterDistancias()

--- Conversoes.cs
+++ Conversoes.cs
@@ -17,9 +17,17 @@ namespace Calculos
             FahrenheitCelsius
         }
 
+        //Tipo de conversão de distâncias
+        public enum ConversaoDistancia
+        {
+            Nulo =0,
+            MetrosMilhas,
+            MilhasMetros
+        }
+
         //Conversão de temperaturas
         // parâmetro conversao é a conversão a efetuar
-        // parâmetro temepratura é a temperatura a converter
+        // parâmetro temperatura é a temperatura a converter
         // return retorna o resultado da conversão
         public static double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
         {
@@ -32,9 +40,34 @@ namespace Calculos
                 return ((temperatura - 32) / 1.8000);
             }
             else
-                return -1;
+            { 
+                return -1; 
+            }
         }
 
+        //Conversão de distâncias
+        // parâmetro conversao é a conversão a efetuar
+        // parâmetro distância é a distânica a converter
+        // return retorna o resultado da conversão da distância
+        public static double ConverterDistancias(ConversaoDistancia conversao, double distancia)
+        {
+            if (conversao == ConversaoDistancia.MetrosMilhas)
+            {
+                return distancia * 0.0006213712;
+            }
+            else if (conversao == ConversaoDistancia.MilhasMetros)
+            {
+                return distancia * 1609.344;
+            }
+            else
+            {
+                return -1;
+            }
+
+        
+        
+        
+        }
 
     }
 }
